0f18dd32d7d7327ba78d926253bb8d0344b4003b
Sean Vela <sean_vela@yahoo.com>
2023-12-19 21:49:28 -0600

    updated project

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -1,15 +1,28 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-const selectMovie = document.getElementById('movie');
+const movieSelect = document.getElementById('movie');
 
-let ticketPrice = +selectMovie.value;
+populateUI();
+
+let ticketPrice = +movieSelect.value;
+
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice)
+{
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 //Update total and count
 function updateSelectedCount()
 {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat)) 
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
 
@@ -17,10 +30,34 @@ function updateSelectedCount()
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+//Get data from local storage and populate UI
+function populateUI()
+{
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats !== null && selectedSeats.length > 0)
+    {
+        seats.forEach((seat, index) =>
+        {
+            if(selectedSeats.indexOf(index) > -1)
+            seat.classList.add('selected');
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+
+    if(selectedMovieIndex !== null)
+    {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+
+}
+
 //Movie select event
-selectMovie.addEventListener('change', (e) =>
+movieSelect.addEventListener('change', (e) =>
 {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
 
     updateSelectedCount();
 })
@@ -35,4 +72,7 @@ container.addEventListener('click', (e) =>
         updateSelectedCount();
     }
 });
+
+//Initial count and total set
+updateSelectedCount();
 
